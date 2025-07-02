6f6554950f4e4164a73c1819b4e7ea25ed0add97
Ivo Vicente <vicenten.junior@unifacol.edu.br>
2025-07-02 12:37:31 -0300

    criação do projeto

---
+++ src/components/views/CriarConta/CriarConta.js
@@ -0,0 +1,16 @@
+import './CriarConta.css';
+
+function CriarConta() {
+
+    return (
+        <div className="CriarConta">
+            <header>
+                <p>Crie a sua conta</p>
+            </header>
+
+            <p>Meu primeiro coponente</p>
+        </div>
+    )
+}
+
+export default CriarConta
